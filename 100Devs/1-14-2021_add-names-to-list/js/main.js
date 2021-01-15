8b4ffb938c07dc627a096f4c5df2d46cf6221502
RussellTAllen <russelltallen@gmail.com>
2021-01-14 20:52:39 -0600

    Update main.js

--- 100Devs/1-14-2021_add-names-to-list/js/main.js
+++ 100Devs/1-14-2021_add-names-to-list/js/main.js
@@ -6,7 +6,7 @@ document.getElementById('button').addEventListener('click', addName)
 
 let names = ["Joe", "Suzy", "Bill", "Frank", "Molly"]
 
-function addName(name){
+function addName(){
     names.push(document.getElementById('name').value)
     document.querySelector('p').innerText = `You are number ${names.length} on the list.  
     \n Here are the current names - ${names.join(", ")}.`
