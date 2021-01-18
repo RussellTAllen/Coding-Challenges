bff72f8dd970dcdf8ef74a24dad9aca8acd20282
RussellTAllen <russelltallen@gmail.com>
2021-01-18 04:47:30 -0600

    More challenges

--- LeetCode/7_reverseInteger.js
+++ LeetCode/7_reverseInteger.js
@@ -1,34 +1,36 @@
 // 7. Reverse Integer - Easy
 
+// RESULTS -    Runtime: 104 ms, faster than 39.95% of JavaScript online submissions for Reverse Integer.
+//              Memory Usage: 40 MB, less than 77.50% of JavaScript online submissions for Reverse Integer.
+
 // Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
 
 // Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
 
 function reverse(x) {
-
+    x = String(x).split("").reverse()
+    if (x[x.length - 1] === "-"){
+        x.splice(x.length -1, 1)
+        x.unshift("-")
+    }
+    x = parseInt(x.join(""))
+    if (x <= Math.pow(-2, 31) || x >= 2**31 - 1) return 0
+    return x
 }
 
-// Example 1:
-
-// Input: x = 123
+console.log(reverse(123))
 // Output: 321
-// Example 2:
-
-// Input: x = -123
+console.log(reverse(-123))
 // Output: -321
-// Example 3:
-
-// Input: x = 120
+console.log(reverse(120))
 // Output: 21
-// Example 4:
-
-// Input: x = 0
+console.log(reverse(0))
 // Output: 0
+console.log(reverse(1))
+// Output: 1
  
 
 // Constraints:
 
-// -231 <= x <= 231 - 1
+// -2^31 <= x <= 2^31 - 1
 
-
-console.log('oh hai!')
