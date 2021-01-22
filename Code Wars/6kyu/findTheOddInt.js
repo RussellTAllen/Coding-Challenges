53f171f1ec372829b5ad745b94ddba9a3184014f
RussellTAllen <russelltallen@gmail.com>
2021-01-22 01:22:34 -0600

    more challenges

--- Code Wars/6kyu/findTheOddInt.js
+++ Code Wars/6kyu/findTheOddInt.js
@@ -5,17 +5,11 @@
 // There will always be only one integer that appears an odd number of times.
 
 function findOdd(arr){
-    let count
+    arr = arr.sort((a,b) => a-b)
+    let count = 0
     for (let i = 0; i < arr.length; i++){
-        count = 1
-        j = i
-        while (arr.includes(arr[i], j +1)){
-            count++
-            j = arr.indexOf(arr[i], j +1)            
-            if (arr[i] === arr[i + 1]) j--
-            arr.splice(j, 1)            
-            if (arr[j+1] === undefined) j = i
-        }if (count % 2 !== 0) return arr[i]
+        count++
+        if ((arr[i] !== arr[i+1]) && (count % 2 !== 0)) return arr[i]
     }
 }
 
@@ -30,28 +24,26 @@ console.log(findOdd([3,4,5,6,7,3,4,5,6]))
 console.log(findOdd([1,1,1,1,5,6,5,6,8,8,2,2,2,1,1]))
 // 2
 
-
-
 // kwlski's solution 
 // function findOdd(arr){
 //     return arr.find((el, index, array) => array.filter((el2) => el2 === el).length % 2 !== 0);
 //   }
 
 //what i would do
-function findOdd(arr){
-    let count;
-    //use the count variable to count the occurences in the array for the currently iterated element by index i
-    for (let i = 0; i < arr.length; i++) {
-      count = 0
-      for (let j = 0; j < arr.length; j++) {
-        if (arr[i] === arr[j]) {
-            count++;
-        }
-      }
-      //now check if count is odd if yes return the current element
-      if (count % 2 !== 0){
-         return arr[i];
-      }
-    }
-    throw new Error("None found");
-}
+// function findOdd(arr){
+//     let count;
+//     //use the count variable to count the occurences in the array for the currently iterated element by index i
+//     for (let i = 0; i < arr.length; i++) {
+//       count = 0
+//       for (let j = 0; j < arr.length; j++) {
+//         if (arr[i] === arr[j]) {
+//             count++;
+//         }
+//       }
+//       //now check if count is odd if yes return the current element
+//       if (count % 2 !== 0){
+//          return arr[i];
+//       }
+//     }
+//     throw new Error("None found");
+// }
