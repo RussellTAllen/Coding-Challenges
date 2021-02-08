ee9904e45db7749efc5f55603145ae145001c92d
RussellTAllen <russelltallen@gmail.com>
2021-02-08 01:37:13 -0600

    more challenges

---
+++ Code Wars/6kyu/breakCamelCase.js
@@ -0,0 +1,38 @@
+// Break Camel Case - 6 kyu
+
+// Complete the solution so that the function will break up camel casing, using a space between words.
+
+// Example
+// solution("camelCasing")  ==  "camel Casing"
+
+function solution(string) {
+    let x = 0
+    let arr = string.split('')
+
+    for (let i = 0; i < string.length; i++){
+        if (string[i] === string[i].toUpperCase()) {
+            arr.splice(i + x, 0, " ")
+            x++
+        }
+    }    
+    return arr.join('')
+}
+
+
+console.log(solution("camelCasing"))
+// "camel Casing"
+console.log(solution('camelCasingTest'))
+// 'camel Casing Test'
+console.log(solution('notcamelcase'))
+// 'notcamelcase'
+
+
+/// This works, but only for the first camel case
+// breakpoint = string.match(regex)
+// breakpoint = string.indexOf(breakpoint)
+// console.log('breakpoint is ' + breakpoint)
+// string = string.split('')
+// console.log(string)
+// string.splice(breakpoint, 0, " ")
+// string = string.join('')
+// string = string.split(' ')
