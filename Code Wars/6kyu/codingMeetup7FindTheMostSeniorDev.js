2b481a79e542708861fe8f5cfb4062068b2f5a7a
RussellTAllen <russelltallen@gmail.com>
2021-03-24 16:02:32 -0500

    more challenges

---
+++ Code Wars/6kyu/codingMeetup7FindTheMostSeniorDev.js
@@ -0,0 +1,66 @@
+// Coding Meetup #7 - Higher Order Functions Series - Find the most senior developer
+
+// You will be given a sequence of objects representing data about developers who have signed up to attend the next coding meetup that you are organising.
+
+// Your task is to return a sequence which includes the developer who is the oldest. In case of a tie, include all same-age senior developers listed in the same order as they appeared in the original input array.
+
+// For example, given the following input array:
+
+// var list1 = [
+//   { firstName: 'Gabriel', lastName: 'X.', country: 'Monaco', continent: 'Europe', age: 49, language: 'PHP' },
+//   { firstName: 'Odval', lastName: 'F.', country: 'Mongolia', continent: 'Asia', age: 38, language: 'Python' },
+//   { firstName: 'Emilija', lastName: 'S.', country: 'Lithuania', continent: 'Europe', age: 19, language: 'Python' },
+//   { firstName: 'Sou', lastName: 'B.', country: 'Japan', continent: 'Asia', age: 49, language: 'PHP' },
+// ];
+// your function should return the following array:
+
+// [
+//   { firstName: 'Gabriel', lastName: 'X.', country: 'Monaco', continent: 'Europe', age: 49, language: 'PHP' },
+//   { firstName: 'Sou', lastName: 'B.', country: 'Japan', continent: 'Asia', age: 49, language: 'PHP' },
+// ]
+// Notes:
+
+// The input array will always be valid and formatted as in the example above.
+
+function findSenior(list) {
+    let result = [list[0]]
+    for (let i = 1; i < list.length; i++){
+        if (result[0].age === list[i].age) result.push(list[i])
+        if (result[0].age < list[i].age) result = [list[i]]
+    }
+    return result
+}
+
+
+var list1 = [
+    { firstName: 'Gabriel', lastName: 'X.', country: 'Monaco', continent: 'Europe', age: 49, language: 'PHP' },
+    { firstName: 'Odval', lastName: 'F.', country: 'Mongolia', continent: 'Asia', age: 38, language: 'Python' },
+    { firstName: 'Emilija', lastName: 'S.', country: 'Lithuania', continent: 'Europe', age: 19, language: 'Python' },
+    { firstName: 'Sou', lastName: 'B.', country: 'Japan', continent: 'Asia', age: 49, language: 'PHP' },
+]
+
+console.log(findSenior(list1)) 
+//   [
+//     { firstName: 'Gabriel', lastName: 'X.', country: 'Monaco', continent: 'Europe', age: 49, language: 'PHP' },
+//     { firstName: 'Sou', lastName: 'B.', country: 'Japan', continent: 'Asia', age: 49, language: 'PHP' },
+//   ]
+
+var list2 = [
+    { firstName: 'Gabriel', lastName: 'X.', country: 'Monaco', continent: 'Europe', age: 49, language: 'PHP' },
+    { firstName: 'Odval', lastName: 'F.', country: 'Mongolia', continent: 'Asia', age: 38, language: 'Python' },
+    { firstName: 'Emilija', lastName: 'S.', country: 'Lithuania', continent: 'Europe', age: 19, language: 'Python' },
+  ];
+  
+console.log(findSenior(list2))
+//   [
+//     { firstName: 'Gabriel', lastName: 'X.', country: 'Monaco', continent: 'Europe', age: 49, language: 'PHP' },
+//   ];
+
+
+
+//////////////
+// Clever solution -
+// function findSenior(list) {
+//     var maxAge = Math.max(...list.map(person => person.age));
+//     return list.filter(person => person.age === maxAge);
+//   }
