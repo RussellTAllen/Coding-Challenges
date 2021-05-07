5284cb6200db4a259341e6ec437905200a77a451
RussellTAllen <russelltallen@gmail.com>
2021-05-06 19:20:22 -0500

    more challenges

---
+++ Code Wars/7kyu/sortArrays1.js
@@ -0,0 +1,10 @@
+// Sort arrays-1 - 7 kyu
+
+// Just a simple sorting usage. Create a function that returns the elements of the input-array / list in a sorted manner.
+
+sortme = function( names ){
+    return names.sort()
+}
+
+console.log(sortme(['one', 'two', 'three' ]))
+// ["one", "three", "two"]
