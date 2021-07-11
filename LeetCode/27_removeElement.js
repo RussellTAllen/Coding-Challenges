2beca7a5aab3dd9ed85c757cae34e075ebfc049d
RussellTAllen <russelltallen@gmail.com>
2021-07-10 22:33:20 -0500

    more challenges

---
+++ LeetCode/27_removeElement.js
@@ -0,0 +1,15 @@
+// 27. Remove Element - Easy
+// https://leetcode.com/problems/remove-element/
+
+var removeElement= function(nums, val) {
+    for (let i = 0, len = nums.length; i < len; i++){
+        if (nums[i] === val) {
+            nums.splice(i, 1)
+            i--
+        }
+    }
+    return nums
+}
+
+console.log(removeElement([3,2,2,3], 3), 2, [2,2])
+console.log(removeElement([0,1,2,2,3,0,4,2], 2), 5, [0,1,4,0,3])
