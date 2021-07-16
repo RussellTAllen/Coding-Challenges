c73ba4576628ceac73a7be22c49e34ed2588213f
RussellTAllen <russelltallen@gmail.com>
2021-07-16 15:16:55 -0500

    more challenges

---
+++ Code Wars/6kyu/countCharactersInYourString.js
@@ -0,0 +1,17 @@
+// Count Characters in your String - 6 kyu
+// https://www.codewars.com/kata/52efefcbcdf57161d4000091/train/javascript
+
+// The main idea is to count all the occurring characters in a string. If you have a string like aba, then the result should be {'a': 2, 'b': 1}.
+
+// What if the string is empty? Then the result should be empty object literal, {}.
+
+function count(str){
+    const result = {}
+    for (let i = 0, len = str.length; i < len; i++){
+        result[str[i]] ? result[str[i]]++ : result[str[i]] = 1
+    }
+    return result
+}
+
+console.log(count("aba"), { a: 2, b: 1 }); 
+console.log(count(""), {});    
