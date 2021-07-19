c357bf4b49aadb486ac0c4f55cc99891aefedff2
RussellTAllen <russelltallen@gmail.com>
2021-07-19 17:01:45 -0500

    more challenges

--- Code Wars/6kyu/highestScoringWord.js
+++ Code Wars/6kyu/highestScoringWord.js
@@ -12,38 +12,47 @@
 
 console.log('a'.charCodeAt() - 96)
 
-function high(x) {
-    let highest = ''
-    let score = 0
-    let highScore = 0
-    let arr = x.split(' ')
-    arr.forEach(w => {
-        score = 0
-        for (let i = 0; i < w.length; i++){
-            score += w[i].charCodeAt() - 96
-        }
-        console.log(`score for ${w} is ${score}; current high: ${highScore}`)
-        if (score > highScore) {
-            highest = w
-            highScore = score
-        }
-    })
-    return highest
+// LONG BRUTE FORCE WAY
+// function high(x) {
+//     let highest = ''
+//     let score = 0
+//     let highScore = 0
+//     let arr = x.split(' ')
+//     arr.forEach(w => {
+//         score = 0
+//         for (let i = 0; i < w.length; i++){
+//             score += w[i].charCodeAt() - 96
+//         }
+//         console.log(`score for ${w} is ${score}; current high: ${highScore}`)
+//         if (score > highScore) {
+//             highest = w
+//             highScore = score
+//         }
+//     })
+//     return highest
+// }
+
+// SLICK METHOD WAY
+function high(str){
+    let values = str.split(' ').map(w => w.split('').reduce((a,c) => a + c.charCodeAt() - 96, 0))
+    return str.split(' ')[values.indexOf(Math.max(...values))]
 }
 
-// console.log(high('man i need a taxi up to ubud'))
-// // => 'taxi'
-// console.log(high('what time are we climbing up the volcano'))
-// // => 'volcano'
-// console.log(high('take me to semynak'))
+console.log(high('climbing'))
+console.log(high('volcano'))
+console.log(high('man i need a taxi up to ubud'))
+// => 'taxi'
+console.log(high('what time are we climbing up the volcano'))
+// => 'volcano'
+console.log(high('take me to semynak'))
 // => 'semynak'
-console.log(high('aa b'))
-// => 'aa'
-console.log(high('b aa'))
-// => 'b'
-console.log(high('bb d'))
-// => 'bb'
-console.log(high('d bb'))
-// => 'd'
-console.log(high('aaa b'))
+// console.log(high('aa b'))
+// // => 'aa'
+// console.log(high('b aa'))
+// // => 'b'
+// console.log(high('bb d'))
+// // => 'bb'
+// console.log(high('d bb'))
+// // => 'd'
+// console.log(high('aaa b'))
 // => 'aaa'
