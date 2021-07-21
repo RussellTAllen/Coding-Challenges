e04d1ff739042a6361894f5d31489d84e781289f
RussellTAllen <russelltallen@gmail.com>
2021-07-21 14:21:51 -0500

    more challenges

--- Code Wars/6kyu/IQtest.js
+++ Code Wars/6kyu/IQtest.js
@@ -4,29 +4,24 @@
 
 // ! Keep in mind that your task is to help Bob solve a real IQ test, which means indexes of the elements start from 1 (not 0)
 
-function iqTest(numbers){
-    let even = 0
-    let odd = 0
-    let result = []
-    numbers = numbers.split(' ')
-    for (let i = 0; i < numbers.length; i++){
-        numbers[i] % 2 === 0 ? even++ : odd++
-    }
-    odd === 1 ? result = numbers.filter(n => n % 2 !== 0) : result = numbers.filter(n => n % 2 === 0)
-    return numbers.indexOf(result.toString()) + 1
-}
-
-// FIRST ATTEMPT
 // function iqTest(numbers){
+//     let even = 0
+//     let odd = 0
+//     let result = []
 //     numbers = numbers.split(' ')
 //     for (let i = 0; i < numbers.length; i++){
-//         if (numbers[i] % 2 !== numbers[i+1] % 2) {
-//             console.log('this is i ' +i)
-//             return (numbers[i] % 2 !== numbers[i+2]) ? i+1 : i+2
-//         }
+//         numbers[i] % 2 === 0 ? even++ : odd++
 //     }
+//     odd === 1 ? result = numbers.filter(n => n % 2 !== 0) : result = numbers.filter(n => n % 2 === 0)
+//     return numbers.indexOf(result.toString()) + 1
 // }
 
+function iqTest(nums){
+    nums = nums.split(' ')
+    let odd = nums.filter(n => n % 2 !== 0)
+    let even = nums.filter(n => n % 2 === 0)
+    return odd.length === 1 ? nums.indexOf(odd[0]) + 1 : nums.indexOf(even[0]) + 1
+}
 
 console.log(iqTest("2 4 7 8 10"))
 // => 3
