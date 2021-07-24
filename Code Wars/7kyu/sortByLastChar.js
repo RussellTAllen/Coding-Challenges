084ff0e4ce03e3f22b7495af1af7e4150b74bc56
RussellTAllen <russelltallen@gmail.com>
2021-07-23 20:23:14 -0500

    more challenges

--- Code Wars/7kyu/sortByLastChar.js
+++ Code Wars/7kyu/sortByLastChar.js
@@ -6,7 +6,7 @@
 // If two words have the same last letter, they returned array should show them in the order they appeared in the given string.
 
 function last(x){
-    return x.split(' ').sort((a,b) => a[a.length-1] < b[b.length-1] ? -1 : 1)
+    return x.split(' ').sort((a,b) => a[a.length-1] > b[b.length-1] ? 1 : -1)
 }
 
 console.log(last('man i need a taxi up to ubud'), ['a', 'need', 'ubud', 'i', 'taxi', 'man', 'to', 'up']);
