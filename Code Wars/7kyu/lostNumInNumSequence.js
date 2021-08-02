1bf46fae51b511740ed764f10659505af6c36f64
RussellTAllen <russelltallen@gmail.com>
2021-08-02 16:30:25 -0500

    more challenges

---
+++ Code Wars/7kyu/lostNumInNumSequence.js
@@ -0,0 +1,21 @@
+// Lost Number in Number Sequence - 7kyu
+// https://www.codewars.com/kata/595aa94353e43a8746000120/train/javascript
+
+// An ordered sequence of numbers from 1 to N is given. One number might have deleted from it, then the remaining numbers were mixed. Find the number that was deleted.
+
+// Example:
+
+// The starting array sequence is [1,2,3,4,5,6,7,8,9]
+// The mixed array with one deleted number is [3,2,4,6,7,8,1,9]
+// Your function should return the int 5.
+// If no number was deleted from the array and no difference with it, your function should return the int 0.
+
+// Note: N may be 1 or less (in the latter case, the first array will be []).
+
+function findDeletedNumber(arr, mixArr){
+    return Number(arr.filter(n => !mixArr.includes(n)))
+}
+
+console.log(findDeletedNumber([1,2,3,4,5], [3,4,1,5]), 2, 'Deletion')
+console.log(findDeletedNumber([1,2,3,4,5,6,7,8,9], [1,9,7,4,6,2,3,8]), 5, 'Deletion')
+console.log(findDeletedNumber([1,2,3,4,5,6,7,8,9], [5,7,6,9,4,8,1,2,3]), 0, 'No deletion')
