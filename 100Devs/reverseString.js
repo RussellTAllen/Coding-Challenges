18f78eb550e3add20cf3cd34dce891b434a1d106
RussellTAllen <russelltallen@gmail.com>
2021-08-10 15:56:14 -0500

    more challenges

---
+++ 100Devs/reverseString.js
@@ -0,0 +1,13 @@
+// Reverse a string without using reverse()
+
+function reverseStr(str){
+    let result = ''
+    for (let i = str.length-1; i>=0; i--){
+        result += str[indexedDB]
+    }
+    return result
+}
+
+console.log(reverseStr('Anna'), 'annA')
+console.log(reverseStr('Banana'), 'ananaB')
+console.log(reverseStr('Leon Noel'), 'leoN noeL')
