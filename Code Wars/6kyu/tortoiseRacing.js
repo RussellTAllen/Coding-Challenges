b969879d6e7c0e5b9e53970ec351a59833d45ba6
RussellTAllen <russelltallen@gmail.com>
2022-01-31 00:24:06 -0600

    more challenges

--- Code Wars/6kyu/tortoiseRacing.js
+++ Code Wars/6kyu/tortoiseRacing.js
@@ -3,40 +3,25 @@
 
 function race(v1, v2, g){
     if (v1 > v2) return null
-    let result = []
 
     let s1 = v1/60/60
     let s2 = v2/60/60
-
-    console.log(s1)
-    console.log(s2)
-
     let p1 = g
     let p2 = 0
-
     let seconds = 0
 
-    while (p1 >= p2){
+    while (p1 > p2){
         p1 += s1
         p2 += s2
         g = p1 - p2
         seconds++
     }
 
-    let hours, minutes, secs
-
-    if (seconds/60 < 60)
-        hours = 0
-    else
-        hours = Math.floor(seconds/60/60)
-    
-        
-    minutes = Math.floor(60 * (seconds/60/60 - Math.floor(seconds/60/60)))
-    secs = Math.floor(60 * (60 * (seconds/60/60 - Math.floor(seconds/60/60)) - minutes))
+    let hours = Math.floor(seconds/60/60)
+    let minutes = Math.floor(60 * (seconds/60/60 - hours))
+    let secs = seconds - (hours*60*60 + minutes*60)
          
-    result.push(hours, minutes, secs-1)
-
-    return result
+    return [hours, minutes, secs]
 }
 
 console.log(race(720, 850, 70), [0, 32, 18])
